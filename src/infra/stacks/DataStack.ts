6937a470904a0a322e384c0d4b55c2951b560205
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 12:33:07 +0300

    🗃️ 🧱 Initial steps with DynamoDB stack

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,7 +1,18 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
+import { getStackSuffix } from "../Utils";
 export class DataStack extends Stack {
+  public readonly SpacesTable: ITable;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    this.SpacesTable = new Table(this, "SpacesTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: `SpacesTable${getStackSuffix(this)}`,
+    });
   }
 }
