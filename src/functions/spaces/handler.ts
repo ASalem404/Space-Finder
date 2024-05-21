e8f41ad9f2ec04294590fe7bc5c15b82a07422ba
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 16:27:39 +0300

    Revert "✨ Add the new space operations to the handler"
    This reverts commit fcf4deca105a7f99e3381eeeff9d43f61855bd4c.

--- src/functions/spaces/handler.ts
+++ src/functions/spaces/handler.ts
@@ -1,41 +1,20 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
 import { postSpace } from "./postSpace";
-import { getSpaces } from "./getSpaces";
-import { updateSpace } from "./updateSpace";
-import { deleteSpace } from "./deleteSpace";
-import { MissingFieldError } from "../../utils/validator";
 
 const ddbClient = new DynamoDBClient({});
 export async function handler(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResultV2> {
-  try {
-    switch (event.httpMethod) {
-      case "POST":
-        const postedRequest = await postSpace(event, ddbClient);
-        return postedRequest;
-      case "GET":
-        const getRequestResult = await getSpaces(event, ddbClient);
-        return getRequestResult;
-      case "PUT":
-        const updatedResult = await updateSpace(event, ddbClient);
-        return updatedResult;
-      case "DELETE":
-        const deletedResult = await deleteSpace(event, ddbClient);
-        return deletedResult;
-      default:
-        break;
-    }
-  } catch (error) {
-    if (error instanceof MissingFieldError)
-      return {
-        statusCode: 400,
-        body: JSON.stringify(error.message),
-      };
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error.message),
-    };
+  switch (event.httpMethod) {
+    case "POST":
+      return await postSpace(event, ddbClient);
+    default:
+      break;
   }
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify("No resource founded"),
+  };
 }
