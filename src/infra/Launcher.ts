4bbfe0262975e9b9a3257816f5ce3f15ce87bce1
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 20:49:11 +0300

    ✨ Add the AuthStack to the app

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -2,6 +2,7 @@ import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 const app = new App();
 const dataStack = new DataStack(app, "DataStack");
@@ -9,6 +10,7 @@ const lambdaStack = new LambdaStack(app, "LambdaStack", {
   SpacesTable: dataStack.SpacesTable,
 });
 
+new AuthStack(app, "AuthStack");
 new ApiStack(app, "ApiStack", {
   SpaceLambdaIntegration: lambdaStack.spacelambdaIntegration,
 });
