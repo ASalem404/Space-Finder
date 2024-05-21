0abe31dbe251e1e056671c35700ff861b46d9892
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 23:35:29 +0300

    🛂 create cognito authorixer and attach it to the space api

--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -1,9 +1,17 @@
 import { Stack, StackProps } from "aws-cdk-lib";
-import { RestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import {
+  RestApi,
+  LambdaIntegration,
+  CognitoUserPoolsAuthorizer,
+  MethodOptions,
+  AuthorizationType,
+} from "aws-cdk-lib/aws-apigateway";
+import { IUserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 interface ApiGatewayIntegration extends StackProps {
   SpaceLambdaIntegration: LambdaIntegration;
+  UserPool: IUserPool;
 }
 
 export class ApiStack extends Stack {
@@ -11,10 +19,31 @@ export class ApiStack extends Stack {
     super(scope, id, props);
 
     const api = new RestApi(this, "RestApi");
+
+    // build the cognito authorizer
+    const authorizer = new CognitoUserPoolsAuthorizer(
+      this,
+      "SpaceApiGatewayAuthorizer",
+      {
+        cognitoUserPools: [props.UserPool],
+        identitySource: "method.request.header.Authorization",
+      }
+    );
+
+    // attach the api to the cognito authorizer
+    authorizer._attachToApi(api);
+
+    // authorizer info to be attach to the resources(function)
+    const optionsWithAuth: MethodOptions = {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: {
+        authorizerId: authorizer.authorizerId,
+      },
+    };
     const resource = api.root.addResource("spaces");
-    resource.addMethod("GET", props.SpaceLambdaIntegration);
-    resource.addMethod("POST", props.SpaceLambdaIntegration);
-    resource.addMethod("PUT", props.SpaceLambdaIntegration);
-    resource.addMethod("DELETE", props.SpaceLambdaIntegration);
+    resource.addMethod("GET", props.SpaceLambdaIntegration, optionsWithAuth);
+    resource.addMethod("POST", props.SpaceLambdaIntegration, optionsWithAuth);
+    resource.addMethod("PUT", props.SpaceLambdaIntegration, optionsWithAuth);
+    resource.addMethod("DELETE", props.SpaceLambdaIntegration, optionsWithAuth);
   }
 }
