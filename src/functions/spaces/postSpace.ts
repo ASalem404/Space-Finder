c73746191994dc6882719901c9fdb55d280f8b52
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 16:21:44 +0300

    🦺 add the input validatin layer

--- src/functions/spaces/postSpace.ts
+++ src/functions/spaces/postSpace.ts
@@ -1,25 +1,27 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
 import { v4 } from "uuid";
+import { validateAsSpaceEntry } from "../../utils/validator";
 export async function postSpace(
   event: APIGatewayProxyEvent,
   ddbClient: DynamoDBClient
 ): Promise<APIGatewayProxyResultV2> {
   const randomId = v4();
   const item = JSON.parse(event.body);
+  item.id = randomId;
+
+  validateAsSpaceEntry(item);
 
   const result = await ddbClient.send(
     new PutItemCommand({
       TableName: process.env.TABLE_NAME,
-      Item: {
-        id: { S: randomId },
-        location: { S: item.location },
-      },
+      Item: marshall(item, { removeUndefinedValues: true }),
     })
   );
 
   return {
     statusCode: 201,
-    body: JSON.stringify({ id: randomId }),
+    body: JSON.stringify({ space: marshall(result.Attributes) }),
   };
 }
