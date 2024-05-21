bc56de7e01278c7104a550bf9ebea0e09cf575b6
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 14:13:22 +0300

    🧱 Create lambda stack with dynamodb policies

---
+++ src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+interface LambdaFunctionProps extends StackProps {
+  SpacesTable: ITable;
+}
+export class LambdaStack extends Stack {
+  public readonly spacelambdaIntegration: LambdaIntegration;
+  constructor(scope: Construct, id: string, props: LambdaFunctionProps) {
+    super(scope, id, props);
+
+    const lambdaFunc = new NodejsFunction(this, "SpaceLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "..", "functions", "spaces", "handler.ts"),
+      environment: {
+        TABLE_NAME: props.SpacesTable.tableName,
+      },
+    });
+
+    lambdaFunc.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["dynamodb:PutItem", "dynamodb:GetItem"],
+        resources: [props.SpacesTable.tableArn],
+      })
+    );
+    this.spacelambdaIntegration = new LambdaIntegration(lambdaFunc);
+  }
+}
