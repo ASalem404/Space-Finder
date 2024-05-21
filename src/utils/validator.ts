a4b2cfdc77a1cd3c10274a66390300e21bce353e
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 17:22:32 +0300

    🥅 Add parsing error class

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -5,6 +5,11 @@ export class MissingFieldError extends Error {
     super(`value for field '${missingField}' is Expected.`);
   }
 }
+export class ParseJsonError extends Error {
+  constructor() {
+    super(`Can't parse the given input`);
+  }
+}
 export function validateAsSpaceEntry(field: any) {
   if (!(field as spaceEntry).location) throw new MissingFieldError("location");
   if (!(field as spaceEntry).name) throw new MissingFieldError("name");
