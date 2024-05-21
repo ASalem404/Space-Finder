1e4b3c96e7dfc93cf305c5a3663862217152f38b
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 16:19:07 +0300

    👔 Add get space by Id function

---
+++ src/functions/spaces/getSpace.ts
@@ -0,0 +1,25 @@
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { APIGatewayProxyResultV2 } from "aws-lambda";
+export async function getSpace(
+  id: string,
+  ddbClient: DynamoDBClient
+): Promise<APIGatewayProxyResultV2> {
+  const result = await ddbClient.send(
+    new GetItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Key: { id: { S: id } },
+    })
+  );
+
+  if (result.Item)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ space: unmarshall(result.Item) }),
+    };
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify("There is no Item with id: " + id),
+  };
+}
