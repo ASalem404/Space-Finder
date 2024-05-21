936d164d643c23b91487ca950b3a431e1e80b217
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-21 16:23:22 +0300

    👔 Add get all spaces function

---
+++ src/functions/spaces/getSpaces.ts
@@ -0,0 +1,23 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+import { getSpace } from "./getSpace";
+export async function getSpaces(
+  event: APIGatewayProxyEvent,
+  ddbClient: DynamoDBClient
+): Promise<APIGatewayProxyResultV2> {
+  if (event.queryStringParameters && "id" in event.queryStringParameters)
+    return await getSpace(event.queryStringParameters.id, ddbClient);
+
+  const result = await ddbClient.send(
+    new ScanCommand({
+      TableName: process.env.TABLE_NAME,
+    })
+  );
+
+  const unmarshalledResult = result.Items.map((item) => unmarshall(item));
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ result: unmarshalledResult }),
+  };
+}
