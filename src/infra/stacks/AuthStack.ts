ff9450e6c47af7ed33d95f50272323996e64c5bf
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-22 18:09:43 +0300

    ✨ Create cognito identity pool

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import {
+  CfnIdentityPool,
   CfnUserPoolGroup,
   UserPool,
   UserPoolClient,
@@ -9,11 +10,13 @@ import { Construct } from "constructs";
 export class AuthStack extends Stack {
   public userPool: UserPool;
   private userPoolClient: UserPoolClient;
+  private identityPool: CfnIdentityPool;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.createUserPool();
     this.createUserPoolClient();
     this.CreateAdminsGroup();
+    this.createIdentityPool();
   }
 
   private createUserPool() {
@@ -48,6 +51,21 @@ export class AuthStack extends Stack {
     new CfnUserPoolGroup(this, "SpaceAdminsGroup", {
       userPoolId: this.userPool.userPoolId,
       groupName: "admins",
+    });
+  }
+
+  private createIdentityPool() {
+    this.identityPool = new CfnIdentityPool(this, "SpaceIdentityPool", {
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: this.userPoolClient.userPoolClientId,
+          providerName: this.userPool.userPoolProviderName,
+        },
+      ],
+    });
+    new CfnOutput(this, "SpaceIdentityPoolId", {
+      value: this.identityPool.ref,
     });
   }
 }
