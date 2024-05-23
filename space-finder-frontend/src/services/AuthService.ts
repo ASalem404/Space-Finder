fd43fbacc53ebbc46c8030a5c1d32078b1a35454
ASalem404 <ahmed.salem.prime@gmail.com>
2024-05-23 20:27:40 +0300

    🛂 add required permission to the user to grant photos bucket access

--- space-finder-frontend/src/services/AuthService.ts
+++ space-finder-frontend/src/services/AuthService.ts
@@ -1,8 +1,23 @@
+// export class AuthService {
+//   public async login(
+//     userName: string,
+//     password: string
+//   ): Promise<Object | undefined> {
+//     return {
+//       user: "abc",
+//     };
+//   }
+
+//   public getUserName() {
+//     return "some user";
+//   }
+// }
+
 import { type CognitoUser } from "@aws-amplify/auth";
 import { Amplify, Auth } from "aws-amplify";
 import { AuthStack } from "../../../cdkOutput.json";
 
-const awsRegion = "eu-west-1";
+const awsRegion = "eu-central-1";
 
 Amplify.configure({
   Auth: {
